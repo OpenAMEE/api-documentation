4f7f5e90798c87b06e226eeef60e855960a8853e
James Smith <james@floppy.org.uk>
2011-02-25 18:05:48 +0000

    Add profile item list operation.

---
+++ src/doc/samples/json/get_profile_category.js
@@ -0,0 +1,392 @@
+{
+  "apiVersion":"2.0",
+  "pager":{
+    "to":2,
+    "lastPage":1,
+    "nextPage":-1,
+    "items":2,
+    "start":0,
+    "itemsFound":2,
+    "requestedPage":1,
+    "currentPage":1,
+    "from":1,
+    "itemsPerPage":10,
+    "previousPage":-1
+  },
+  "profileCategories":[],
+  "dataCategory":{
+    "uid":"A92693A99BAD",
+    "dataCategory":{
+      "uid":"30BA55A0C472",
+      "name":"Energy",
+      "path":"energy"
+    },
+    "environment":{
+      "uid":"5F5887BCF726",
+      "itemsPerFeed":10,
+      "description":"",
+      "name":"AMEE",
+      "owner":"",
+      "path":"",
+      "itemsPerPage":10
+    },
+    "created":"2007-07-27 09:30:44.0",
+    "name":"Quantity",
+    "path":"quantity",
+    "itemDefinition":{
+      "uid":"212C818D8F16",
+      "environment":{
+        "uid":"5F5887BCF726"
+      },
+      "created":"2007-07-27 09:30:44.0",
+      "name":"Energy Quantity",
+      "drillDown":"type",
+      "modified":"2007-07-27 09:30:44.0"
+    },
+    "modified":"2007-07-27 09:30:44.0"
+  },
+  "environment":{
+    "uid":"5F5887BCF726",
+    "itemsPerFeed":10,
+    "created":"2007-07-27 09:30:44.0",
+    "description":"",
+    "name":"AMEE",
+    "owner":"",
+    "path":"",
+    "itemsPerPage":10,
+    "modified":"2007-07-27 09:30:44.0"
+  },
+  "totalAmount":{
+    "unit":"kg/year",
+    "value":"624.660000"
+  },
+  "path":"/home/energy/quantity",
+  "profileItemActions":{
+    "allowCreate":true,
+    "allowView":true,
+    "allowList":true,
+    "allowModify":true,
+    "allowDelete":true
+  },
+  "actions":{
+    "allowCreate":true,
+    "allowView":true,
+    "allowList":true,
+    "allowModify":true,
+    "allowDelete":true
+  },
+  "profileItems":[
+    {
+      "amount":{
+        "unit":"kg/year",
+        "value":24.66
+      },
+      "uid":"91C8D5EA8287",
+      "startDate":"2009-03-18T10:24:00Z",
+      "itemValues":[
+        {
+          "itemValueDefinition":{
+            "uid":"E0EFED6FD7E6",
+            "name":"Payment frequency",
+            "path":"paymentFrequency",
+            "valueDefinition":{
+              "uid":"CCEB59CACE1B",
+              "environment":{
+                "uid":"5F5887BCF726"
+              },
+              "created":"2007-07-27 09:30:44.0",
+              "description":"",
+              "name":"text",
+              "valueType":"TEXT",
+              "modified":"2007-07-27 09:30:44.0"
+            }
+          },
+          "perUnit":"",
+          "uid":"664A7C527788",
+          "unit":"",
+          "name":"Payment frequency",
+          "value":"",
+          "path":"paymentFrequency",
+          "displayPath":"paymentFrequency",
+          "displayName":"Payment frequency"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"646C9F2E7BFD",
+          "unit":"",
+          "name":"Green tariff",
+          "value":"",
+          "path":"greenTariff",
+          "displayPath":"greenTariff",
+          "displayName":"Green tariff"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"DF24821FCAFF",
+          "unit":"",
+          "name":"Season",
+          "value":"",
+          "path":"season",
+          "displayPath":"season",
+          "displayName":"Season"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"E81D893DA770",
+          "unit":"",
+          "name":"Includes Heating",
+          "value":"false",
+          "path":"includesHeating",
+          "displayPath":"includesHeating",
+          "displayName":"Includes Heating"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"0A7A75228E75",
+          "unit":"kg",
+          "name":"Mass Per Time",
+          "value":"0",
+          "path":"massPerTime",
+          "displayPath":"massPerTime",
+          "displayName":"Mass Per Time"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"month",
+          "uid":"449497EFDDA8",
+          "unit":"kWh",
+          "name":"Energy Consumption",
+          "value":"10",
+          "path":"energyConsumption",
+          "displayPath":"energyConsumption",
+          "displayName":"Energy Consumption"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"D748A6115D46",
+          "unit":"kWh",
+          "name":"Current Reading",
+          "value":"0",
+          "path":"currentReading",
+          "displayPath":"currentReading",
+          "displayName":"Current Reading"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"33A42602015A",
+          "unit":"kWh",
+          "name":"Last Reading",
+          "value":"0",
+          "path":"lastReading",
+          "displayPath":"lastReading",
+          "displayName":"Last Reading"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"1A64F4928A5A",
+          "unit":"L",
+          "name":"Volume Per Time",
+          "value":"0",
+          "path":"volumePerTime",
+          "displayPath":"volumePerTime",
+          "displayName":"Volume Per Time"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"0D3F2548F2C8",
+          "unit":"",
+          "name":"Number of deliveries",
+          "value":"",
+          "path":"deliveries",
+          "displayPath":"deliveries",
+          "displayName":"Number of deliveries"
+        }
+      ],
+      "created":"2009-03-18T10:24:44Z",
+      "name":"test1",
+      "endDate":"",
+      "dataItem":{
+        "uid":"66056991EE23",
+        "Label":"gas"
+      },
+      "modified":"2009-03-18T10:24:45Z"
+    },
+    {
+      "amount":{
+        "unit":"kg/year",
+        "value":600
+      },
+      "uid":"8D91B1B3AA7F",
+      "startDate":"2009-03-18T10:24:00Z",
+      "itemValues":[
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"9AE21CAE9FB9",
+          "unit":"",
+          "name":"Payment frequency",
+          "value":"",
+          "path":"paymentFrequency",
+          "displayPath":"paymentFrequency",
+          "displayName":"Payment frequency"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"27BEDBFA4B95",
+          "unit":"",
+          "name":"Green tariff",
+          "value":"",
+          "path":"greenTariff",
+          "displayPath":"greenTariff",
+          "displayName":"Green tariff"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"B62345BF0C1B",
+          "unit":"",
+          "name":"Season",
+          "value":"",
+          "path":"season",
+          "displayPath":"season",
+          "displayName":"Season"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"",
+          "uid":"ECD08626DFD4",
+          "unit":"",
+          "name":"Includes Heating",
+          "value":"false",
+          "path":"includesHeating",
+          "displayPath":"includesHeating",
+          "displayName":"Includes Heating"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"950BE1E365F5",
+          "unit":"kg",
+          "name":"Mass Per Time",
+          "value":"0",
+          "path":"massPerTime",
+          "displayPath":"massPerTime",
+          "displayName":"Mass Per Time"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"3C73CEC8013A",
+          "unit":"kWh",
+          "name":"Energy Consumption",
+          "value":"0",
+          "path":"energyConsumption",
+          "displayPath":"energyConsumption",
+          "displayName":"Energy Consumption"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"14DF0C9B48EA",
+          "unit":"kWh",
+          "name":"Current Reading",
+          "value":"0",
+          "path":"currentReading",
+          "displayPath":"currentReading",
+          "displayName":"Current Reading"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"3651AB0B3DEB",
+          "unit":"kWh",
+          "name":"Last Reading",
+          "value":"0",
+          "path":"lastReading",
+          "displayPath":"lastReading",
+          "displayName":"Last Reading"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"month",
+          "uid":"0187E2D7F44C",
+          "unit":"L",
+          "name":"Volume Per Time",
+          "value":"20",
+          "path":"volumePerTime",
+          "displayPath":"volumePerTime",
+          "displayName":"Volume Per Time"
+        },
+        {
+          "itemValueDefinition":{
+            ...
+          },
+          "perUnit":"year",
+          "uid":"800F5A8C68A0",
+          "unit":"",
+          "name":"Number of deliveries",
+          "value":"",
+          "path":"deliveries",
+          "displayPath":"deliveries",
+          "displayName":"Number of deliveries"
+        }
+      ],
+      "created":"2009-03-18T10:24:46Z",
+      "name":"test2",
+      "endDate":"",
+      "dataItem":{
+        "uid":"878854C275BC",
+        "Label":"biodiesel"
+      },
+      "modified":"2009-03-18T10:24:46Z"
+    }
+  ],
+  "profile":{
+    "uid":"F554D93A62AB"
+  }
+}
