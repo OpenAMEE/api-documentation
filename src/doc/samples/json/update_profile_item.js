527b70d193a9766babd9eae4c95527880e052425
James Smith <james@floppy.org.uk>
2011-02-25 13:38:06 +0000

    Add PUT profileitem

---
+++ src/doc/samples/json/update_profile_item.js
@@ -0,0 +1,190 @@
+{
+  "apiVersion":"2.0",
+  "profileItem":{
+    "uid":"D47C465B8157",
+    "itemValues":[
+      {
+        "itemValueDefinition":{
+          "uid":"F3900EDAB5EB",
+          "name":"Country",
+          "path":"country",
+          "valueDefinition":{
+            "uid":"CCEB59CACE1B",
+            "environment":{
+              "uid":"5F5887BCF726"
+            },
+            "created":"2007-07-27 09:30:44.0",
+            "description":"",
+            "name":"text",
+            "valueType":"TEXT",
+            "modified":"2007-07-27 09:30:44.0"
+          }
+        },
+        "perUnit":"",
+        "uid":"01D8AD934852",
+        "unit":"",
+        "name":"Country",
+        "value":"",
+        "path":"country",
+        "displayPath":"country",
+        "displayName":"Country"
+      },
+      {
+        "itemValueDefinition":{
+          "uid":"85DAD143CD22",
+          "name":"Occupants",
+          "path":"occupants",
+          "valueDefinition":{
+            "uid":"45433E48B39F",
+            "environment":{
+              "uid":"5F5887BCF726"
+            },
+            "created":"2007-07-27 09:30:44.0",
+            "description":"",
+            "name":"amount",
+            "valueType":"DECIMAL",
+            "modified":"2007-07-27 09:30:44.0"
+          }
+        },
+        "perUnit":"",
+        "uid":"FB45DF9BC8FC",
+        "unit":"",
+        "name":"Occupants",
+        "value":"-1",
+        "path":"occupants",
+        "displayPath":"occupants",
+        "displayName":"Occupants"
+      },
+      {
+        "itemValueDefinition":{
+          "perUnit":"year",
+          "uid":"5898B35AE183",
+          "unit":"km",
+          "name":"Distance",
+          "path":"distance",
+          "valueDefinition":{
+            "uid":"45433E48B39F",
+            "environment":{
+              "uid":"5F5887BCF726"
+            },
+            "created":"2007-07-27 09:30:44.0",
+            "description":"",
+            "name":"amount",
+            "valueType":"DECIMAL",
+            "modified":"2007-07-27 09:30:44.0"
+          }
+        },
+        "perUnit":"month",
+        "uid":"557784471D55",
+        "unit":"km",
+        "name":"Distance",
+        "value":"200",
+        "path":"distance",
+        "displayPath":"distance",
+        "displayName":"Distance"
+      },
+      {
+        "itemValueDefinition":{
+          "perUnit":"L",
+          "uid":"2538ABD1FFE7",
+          "unit":"km",
+          "name":"Fuel Consumption",
+          "path":"fuelConsumption",
+          "valueDefinition":{
+            "uid":"45433E48B39F",
+            "environment":{
+              "uid":"5F5887BCF726"
+            },
+            "created":"2007-07-27 09:30:44.0",
+            "description":"",
+            "name":"amount",
+            "valueType":"DECIMAL",
+            "modified":"2007-07-27 09:30:44.0"
+          }
+        },
+        "perUnit":"L",
+        "uid":"905DAF5F5008",
+        "unit":"km",
+        "name":"Fuel Consumption",
+        "value":"0",
+        "path":"fuelConsumption",
+        "displayPath":"fuelConsumption",
+        "displayName":"Fuel Consumption"
+      },
+      {
+        "itemValueDefinition":{
+          "perUnit":"L",
+          "uid":"FBFB386F5FBB",
+          "unit":"km",
+          "name":"Own Fuel Consumption",
+          "path":"ownFuelConsumption",
+          "valueDefinition":{
+            "uid":"45433E48B39F",
+            "environment":{
+              "uid":"5F5887BCF726"
+            },
+            "created":"2007-07-27 09:30:44.0",
+            "description":"",
+            "name":"amount",
+            "valueType":"DECIMAL",
+            "modified":"2007-07-27 09:30:44.0"
+          }
+        },
+        "perUnit":"L",
+        "uid":"F3F38381BFD8",
+        "unit":"km",
+        "name":"Own Fuel Consumption",
+        "value":"0",
+        "path":"ownFuelConsumption",
+        "displayPath":"ownFuelConsumption",
+        "displayName":"Own Fuel Consumption"
+      }
+    ],
+    "dataCategory":{
+      "uid":"E13720699F84",
+      "name":"Generic",
+      "path":"generic"
+    },
+    "startDate":"2009-03-19T00:00:00Z",
+    "itemDefinition":{
+      "uid":"253122C933E1",
+      "name":"Motorcycle Generic",
+      "drillDown":"fuel, size"
+    },
+    "endDate":"",
+    "dataItem":{
+      "uid":"31449121F8B8",
+      "Label":"petrol, large"
+    },
+    "modified":"2009-03-19T17:38:50Z",
+    "amount":{
+      "unit":"kg/year",
+      "value":308.64
+    },
+    "environment":{
+      "uid":"5F5887BCF726",
+      "itemsPerFeed":10,
+      "description":"",
+      "name":"AMEE",
+      "owner":"",
+      "path":"",
+      "itemsPerPage":10
+    },
+    "created":"2009-03-19T17:38:50Z",
+    "name":"test1",
+    "profile":{
+      "uid":"155DD3C63646"
+    }
+  },
+  "path":"/transport/motorcycle/generic/D47C465B8157",
+  "actions":{
+    "allowCreate":true,
+    "allowView":true,
+    "allowList":true,
+    "allowModify":true,
+    "allowDelete":true
+  },
+  "profile":{
+    "uid":"155DD3C63646"
+  }
+}
