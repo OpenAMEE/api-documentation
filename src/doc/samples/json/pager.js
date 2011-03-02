f3716c82e93d3f796bf19186d56c77f3bad4a253
James Smith <james@floppy.org.uk>
2011-03-02 17:04:56 +0000

    Add paging reference

---
+++ src/doc/samples/json/pager.js
@@ -0,0 +1,13 @@
+"pager": {
+  "items": 13,
+  "currentPage": 1,
+  "itemsPerPage": 10,
+  "from": 1,
+  "nextPage": 2,
+  "to": 10,
+  "itemsFound": 10,
+  "start": 0,
+  "requestedPage": 1,
+  "lastPage": 2,
+  "previousPage": -1
+},
