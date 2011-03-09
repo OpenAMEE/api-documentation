6b46453d3bf42339236d32e273384d9092c345fc
James Smith <james@floppy.org.uk>
2011-03-09 11:50:07 +0000

    update deploy config to go to flood. OPS-1080

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,10 @@ set :deploy_via, :copy
 set :copy_compression,    :gzip
 
 set :user,  'rails'
-set :deploy_to, '/data/www/documentation'
+set :deploy_to, '/var/www/apps/documentation'
 
-role :web, "blizzard.amee.com"
-role :app, "blizzard.amee.com"
+role :web, "flood.amee.com"
+role :app, "flood.amee.com"
 
 namespace :deploy do
   [:restart, :start, :stop].each do |t|
